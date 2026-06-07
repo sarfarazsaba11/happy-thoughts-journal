ea627e68b2a761aa5e31374eddc9bd30309c69ba
Saba Sarfaraz <sarfarazsaba11@gmail.com>
2026-06-07 20:35:45 +0500

    fix: use nitro vercel preset

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart({
-      server: { entry: "src/server.ts" },
+      server: {
+        entry: "src/server.ts",
+        preset: "vercel", // 👈 this is all you need
+      },
       tsr: {
         routesDirectory: "./src/routes",
         generatedRouteTree: "./src/routeTree.gen.ts",
